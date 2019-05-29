3abf85af780c91468167a9e96a0dfa29b8a44d55
klaus <oriokiklaus@gmail.com>
2019-05-29 10:04:07 +0300

    add view function to index template

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse,Http404
 
 # Create your views here.
+def home(request):
+    return render(request, 'index.html')
