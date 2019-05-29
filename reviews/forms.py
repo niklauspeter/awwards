a84e369177b4fc17f2f44c698eda8866f1da6b33
klaus <oriokiklaus@gmail.com>
2019-05-29 16:17:06 +0300

    add a form for adding  new project post

---
+++ reviews/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Project
+
+class NewsLetterForm(forms.Form):
+    your_name = forms.CharField(label='First Name',max_length=30)
+    email = forms.EmailField(label='Email')
+
+class NewProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        exclude = ['user', 'project_url']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
