857908cd39e48e06849ab30ad4f53923c26ee464
klaus <oriokiklaus@gmail.com>
2019-05-30 10:33:06 +0300

    add urlsconf for the search form as well as one for accounts authentication

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,6 +1,14 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.home,name = 'welcome'),
+    url('^$',views.home,name = 'home'),
+    url(r'^project/(\d+)',views.project,name ='project'),
+    url(r'^new/project$', views.new_project, name='new-project'),
+     url(r'^accounts/profile/',views.profile,name ='profile'),
+     url(r'^search/', views.search_results, name='search_results'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
